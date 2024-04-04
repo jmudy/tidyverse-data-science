e37a55387ccf93a1ce403a98e6c8e1d87bd5f4f6
jmudy <62428273+jmudy@users.noreply.github.com>
2024-04-04 22:53:07 +0200

    fix ortografia

--- scripts/Tema 6 - Limpiando y Dando Estructura a los Datos/01-tidy-data.R
+++ scripts/Tema 6 - Limpiando y Dando Estructura a los Datos/01-tidy-data.R
@@ -1,11 +1,16 @@
 
 library(tidyverse)
 
+table <- read_csv("../data/population.csv")
+View(table)
 
+table %>%
+  mutate(rate = cases/population*1e5)
 
+table %>%
+  count(year, wt = cases)
 
-
-
-
-
-
+table %>%
+  ggplot(aes(year, cases)) +
+  geom_line(aes(group = country), color = "grey") +
+  geom_point(aes(color = country))
